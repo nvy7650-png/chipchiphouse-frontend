3f654c4675d04c321f3339fa14428f9919a2aea1
Vy <nvy7650@gmail.com>
2026-08-25 22:51:09 +0700

    install cloudinary

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,27 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+
 import AdminDashboard from './pages/AdminDashboard';
+import AdminCategories from './pages/AdminCategories';
 import AdminGroups from './pages/AdminGroups';
 
 export default function App() {
   return (
     <BrowserRouter>
-
       <Routes>
 
+        {/* CLIENT */}
         <Route path="/" element={<HomePage />} />
-
         <Route path="/login" element={<LoginPage />} />
-
         <Route path="/register" element={<RegisterPage />} />
 
+        {/* ADMIN */}
         <Route path="/admin" element={<AdminDashboard />} />
-
-        <Route
-          path="/admin/groups"
-          element={<AdminGroups />}
-        />
+        <Route path="/admin/categories" element={<AdminCategories />} />
+        <Route path="/admin/groups" element={<AdminGroups />} />
 
       </Routes>
-
     </BrowserRouter>
   );
 }
